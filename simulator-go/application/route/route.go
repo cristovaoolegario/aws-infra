8c76d9ec642d920041872d9e1c8948fe531cbfd7
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-10-19 16:02:13 -0300

    chore(simulator): add route file and first tests

---
+++ simulator-go/application/route/route.go
@@ -0,0 +1,85 @@
+package route
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Route struct {
+	ID        string     `json:"routeId"`
+	ClientID  string     `json:"clientId"`
+	Positions []Position `json:"position"`
+}
+
+type Position struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"log"`
+}
+
+type PartialRoutePosition struct {
+	ID       string    `json:"routeId"`
+	ClientID string    `json:"clientId"`
+	Position []float64 `json:"position"`
+	Finished bool      `json:"finished"`
+}
+
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("route id not informed")
+	}
+	f, err := os.Open("destinations/" + r.ID + ".txt")
+
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return nil
+		}
+		long, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			return nil
+		}
+		r.Positions = append(r.Positions, Position{
+			Latitude:  lat,
+			Longitude: long,
+		})
+	}
+
+	return nil
+}
+
+func (r *Route) ExportJsonPositions() ([]string, error) {
+	var route PartialRoutePosition
+	var result []string
+	total := len(r.Positions)
+
+	for k, v := range r.Positions {
+		route.ID = r.ID
+		route.ClientID = r.ClientID
+		route.Position = []float64{v.Latitude, v.Longitude}
+		route.Finished = false
+		if total-1 == k {
+			route.Finished = true
+		}
+		jsonRoute, err := json.Marshal(route)
+
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, string(jsonRoute))
+	}
+
+	return result, nil
+}
---
+++ simulator-go/application/route/route_test.go
@@ -0,0 +1,58 @@
+package route
+
+import "testing"
+
+func TestLoadPositions(t *testing.T) {
+	t.Run("Should return error When there is no route ID", func(t *testing.T) {
+		route := Route{
+			ID:        "",
+			ClientID:  "",
+			Positions: []Position{},
+		}
+
+		err := route.LoadPositions()
+
+		if err == nil {
+			t.Errorf("Expected an error")
+		}
+		if err.Error() != "route id not informed" {
+			t.Errorf("Expected an error: 'route id not informed'")
+		}
+	})
+
+	t.Run("Should return error when there is no destination file to open", func(t *testing.T) {
+		route := Route{
+			ID:        "5",
+			ClientID:  "1",
+			Positions: []Position{},
+		}
+
+		err := route.LoadPositions()
+
+		if err == nil {
+			t.Errorf("Expected an error")
+		}
+		if err.Error() != "open destinations/1.txt: no such file or directory" {
+			t.Errorf("Expected an error: 'open destinations/1.txt: no such file or directory', got: %v", err.Error())
+		}
+	})
+
+	t.Run("Should read file and load positions properly", func(t *testing.T) {
+		route := Route{
+			ID:        "1",
+			ClientID:  "1",
+			Positions: []Position{},
+		}
+
+		err := route.LoadPositions()
+
+		if err != nil {
+			t.Errorf("An Unexpected error occurs: %v", err.Error())
+		}
+
+	})
+}
+
+func TestExportJsonPositions(t *testing.T) {
+	//TODO
+}
